b36563f7b3c403655f502077d82c351e1323544c
Roman Kuzero <rombeldan@gmail.com>
2024-02-23 20:48:40 +0200

    Finish the basic logic

--- project.js
+++ project.js
@@ -4,15 +4,15 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-  A: 2,
-  B: 4,
-  C: 6,
-  D: 8,
+  A: 4,
+  B: 6,
+  C: 8,
+  D: 10,
 };
 
 const SYMBOL_VALUES = {
-  A: 5,
-  B: 4,
+  A: 10,
+  B: 5,
   C: 3,
   D: 2,
 };
@@ -80,10 +80,75 @@ const spin = () => {
 };
 
 const transpose = (reels) => {
+  const rows = [];
 
-}
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin()
+  return rows;
+};
+
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = '';
+    for ([i, symbol] of row.entries()) {
+      rowString += symbol;
+      if (i != row.length - 1) rowString += ' | ';
+    }
+    console.log(rowString);
+  }
+};
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+
+  return winnings;
+};
+
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log('You have a balance of $' + balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+    balance += winnings;
+    console.log('You won, $' + winnings.toString());
+
+    if (balance <= 0) {
+      console.log('You ran out of money :(');
+      break;
+    }
+
+    const playAgain = prompt('Do you want to play again (y/n)? ');
+
+    if (playAgain != 'y') break;
+  }
+};
+
+game();
